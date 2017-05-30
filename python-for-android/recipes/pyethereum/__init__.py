68197fca0dc70502bebc7da6346b71b4a973717f
Andre Miras <andre.miras@elementsinteractive.es>
2017-05-30 18:57:43 +0000

    PyethereumRecipe update
    Explicitly patching rather than forking

--- python-for-android/recipes/pyethereum/__init__.py
+++ python-for-android/recipes/pyethereum/__init__.py
@@ -3,15 +3,14 @@ from pythonforandroid.recipe import PythonRecipe
 
 class PyethereumRecipe(PythonRecipe):
 
-    # using custom fork, for fixing a pyethereum upstream bug:
-    # https://github.com/ethereum/pyethereum/pull/731
-    # url = 'https://github.com/ethereum/pyethereum/archive/develop.zip'
-    url = 'https://github.com/AndreMiras/pyethereum/archive/patch-1.zip'
+    url = 'https://github.com/ethereum/pyethereum/archive/develop.zip'
 
     depends = [
         'python2','setuptools', 'pycryptodome', 'pysha3', 'ethash', 'scrypt'
     ]
 
     call_hostpython_via_targetpython = False
+
+    patches = ["bug731.patch"]
 
 recipe = PyethereumRecipe()
