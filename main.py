07e69c71eeac558bf9f8f6c037e452b47cf94b9e
Andre Miras <andre.miras@gmail.com>
2017-06-12 01:19:49 +0200

    Using KivyMD, updated style & layout

--- main.py
+++ main.py
@@ -6,19 +6,31 @@ kivy.require('1.10.0')
 from kivy.uix.floatlayout import FloatLayout
 from kivy.app import App
 from kivy.properties import ObjectProperty, StringProperty
+from kivymd.theming import ThemeManager
+from kivy.clock import Clock
+
+
 import pywalib
 
 
 class Controller(FloatLayout):
+
     balance_label = ObjectProperty()
 
-    def do_action(self):
+    def __init__(self, **kwargs):
+        super(Controller, self).__init__(**kwargs)
+        Clock.schedule_once(self._load_balance)
+
+    def _load_balance(self, dt=None):
         account = pywalib.get_main_account()
         balance = pywalib.get_balance(account.address.encode("hex"))
-        self.balance_label.text = 'Balance: %s' % balance
+        overview_id = self.ids.overview_id
+        balance_label_id = overview_id.ids.balance_label_id
+        balance_label_id.text = 'Balance: %s' % balance
 
 
 class ControllerApp(App):
+    theme_cls = ThemeManager()
 
     def build(self):
         return Controller(info='Hello world')
