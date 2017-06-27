723156591d572b13feee06010ec89f964a75f90b
Andre Miras <andre.miras@gmail.com>
2017-06-28 00:13:17 +0200

    Add set -e to tools.sh, refs #35

--- travis/tools.sh
+++ travis/tools.sh
@@ -19,6 +19,9 @@ dump_output
 kill $PING_LOOP_PID
 END
 
+# exits on fail
+set -e
+
 export PING_SLEEP=30s
 export WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export BUILD_OUTPUT=$WORKDIR/build.out
