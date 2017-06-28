b38266471da78761cf5c1a18c8b73f7deb59ec53
Andre Miras <andre.miras@gmail.com>
2017-06-28 22:39:33 +0200

    Trying no-cython-compile flag

--- travis/script_android.sh
+++ travis/script_android.sh
@@ -4,6 +4,6 @@
 # exits on fail
 set -e
 
-sudo pip install --upgrade cython==0.21
+sudo pip install --install-option="--no-cython-compile" cython==0.21
 sudo pip install --upgrade buildozer
 ./travis/buildozer_android.sh
