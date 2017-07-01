8ded525e320a58d23677ff5f0b5bdb16da573970
Andre Miras <andre.miras@gmail.com>
2017-07-01 21:48:09 +0200

    agr

--- travis/script_linux.sh
+++ travis/script_linux.sh
@@ -9,7 +9,7 @@ virtualenv --python=python2.7 --no-site-packages venv
 source venv/bin/activate
 
 # installs requirements
-# pip install --install-option="--no-cython-compile" $(grep Cython requirements/requirements.txt)
+pip install --install-option="--no-cython-compile" $(grep Cython requirements/requirements.txt)
 pip install -r requirements/test_requirements.txt
 pip install -r requirements.txt
 
@@ -17,4 +17,4 @@ pip install -r requirements.txt
 # isort --check-only --recursive src/
 # flake8 src/
 # python -m unittest discover --start-directory=src/
-deactivate
+# deactivate
