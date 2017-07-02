c43660db6d408c104c739d07ebce65f414936167
Andre Miras <andre.miras@gmail.com>
2017-07-02 12:57:27 +0200

    Minus Cython

--- travis/script_linux.sh
+++ travis/script_linux.sh
@@ -9,7 +9,7 @@ virtualenv --python=python2.7 --no-site-packages venv
 source venv/bin/activate
 
 # installs requirements
-pip install --install-option="--no-cython-compile" $(grep Cython requirements/requirements.txt)
+# pip install --install-option="--no-cython-compile" $(grep Cython requirements/requirements.txt)
 pip install -r requirements/test_requirements.txt
 pip install -r requirements.txt
 
