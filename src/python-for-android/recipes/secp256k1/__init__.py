c88a4f2a7b6c1d6101ed36b08561a13ae1a7f53b
Andre Miras <andre.miras@gmail.com>
2017-07-03 18:15:56 +0200

    debugging the issue WiP

--- src/python-for-android/recipes/secp256k1/__init__.py
+++ src/python-for-android/recipes/secp256k1/__init__.py
@@ -1,3 +1,4 @@
+from os.path import join
 from pythonforandroid.recipe import PythonRecipe
 
 
@@ -13,7 +14,21 @@ class Secp256k1Recipe(PythonRecipe):
 
     patches = [
         "cross_compile.patch", "drop_setup_requires.patch",
-        "pkg-config.patch", "find_lib.patch"]
+        "pkg-config.patch", "find_lib.patch", "debug.patch"]
+
+    def get_recipe_env(self, arch=None):
+        env = super(Secp256k1Recipe, self).get_recipe_env(arch)
+        libsecp256k1 = self.get_recipe('libsecp256k1', self.ctx)
+        libsecp256k1_dir = libsecp256k1.get_build_dir(arch.arch)
+        env['PYTHON_ROOT'] = self.ctx.get_python_install_dir()
+        env['CFLAGS'] = ' -I' + join(libsecp256k1_dir, 'include')
+        env['CFLAGS'] += ' -I' + env['PYTHON_ROOT'] + '/include/python2.7'
+        env['LDSHARED'] = env['CC'] + \
+            ' -pthread -shared -Wl,-O1 -Wl,-Bsymbolic-functions'
+        env['LDFLAGS'] += ' -L{}'.format(libsecp256k1_dir)
+        # TODO: hardcoded Python version
+        env['LDFLAGS'] += " -landroid -lpython2.7 -lsecp256k1"
+        return env
 
 
 recipe = Secp256k1Recipe()
