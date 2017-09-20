38af8ef53d6ff2da58b3ac9c91f6d3e19f662dad
Andre Miras <andre.miras@gmail.com>
2017-09-20 20:39:27 +0200

    Update sync Receive address alias update
    Makes sure the Receive address widget gets updated when the
    alias is updated, refs #87

--- src/main.py
+++ src/main.py
@@ -278,22 +278,38 @@ class Receive(BoxLayout):
 
     def setup(self):
         """
-        Binds Controller.current_account property.
+        Binds Controller current_account and on_alias_updated.
         """
         self.controller = App.get_running_app().controller
         self.controller.bind(current_account=self.setter('current_account'))
+        self.controller.bind(on_alias_updated=self.on_alias_updated)
         # triggers the update
         self.current_account = self.controller.current_account
 
     def show_address(self, address):
         self.ids.qr_code_id.data = address
 
-    def on_current_account(self, instance, account):
+    def update_address_property(self):
+        """
+        Updates address_property from current_account.
+        """
+        account = self.current_account
         address = "0x" + account.address.encode("hex")
         self.address_property = address
 
+    def on_current_account(self, instance, account):
+        self.update_address_property()
+
     def on_address_property(self, instance, value):
         self.show_address(value)
+
+    def on_alias_updated(self, instance, alias):
+        """
+        Forces account string update, which will triggers an
+        AddressButton.on_address_property event.
+        """
+        self.address_property = ""
+        self.update_address_property()
 
 
 class History(BoxLayout):
