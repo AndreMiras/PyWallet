3ab12516050d2c2722968ff3cd257e211f2357dd
Andre Miras <andre.miras@gmail.com>
2017-10-17 00:05:28 +0200

    bind/unbind camera workaround

--- src/main.py
+++ src/main.py
@@ -951,12 +951,26 @@ class FlashQrCodeScreen(Screen):
         """
         self.controller = App.get_running_app().controller
         self.zbarcam = self.ids.zbarcam_id
+
+    def bind_on_symbols(self):
+        """
+        Since the camera doesn't seem to stop properly, we always bind/unbind
+        on_pre_enter/on_pre_leave.
+        """
         self.zbarcam.bind(symbols=self.on_symbols)
 
+    def unbind_on_symbols(self):
+        """
+        Since the camera doesn't seem to stop properly, makes sure at least
+        events are unbound.
+        """
+        self.zbarcam.unbind(symbols=self.on_symbols)
+
     def on_symbols(self, instance, symbols):
-        # TODO:
-        #   - popup an error if multiple symbols were found
-        #   - verify the address is valid
+        # also ignores if more than 1 code were found since we don't want to
+        # send to the wrong one
+        if len(symbols) != 1:
+            return
         symbol = symbols[0]
         # update Send screen address
         self.controller.send.send_to_address = symbol.data
