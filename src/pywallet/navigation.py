74df2574c1ddd7a8fbde62f85da4b10bcb1fc08d
Andre Miras <andre.miras@gmail.com>
2017-12-11 20:49:43 +0100

    Moved Navigation widget, refs #124

--- src/pywallet/navigation.py
+++ src/pywallet/navigation.py
@@ -5,6 +5,11 @@ from kivy.properties import StringProperty
 from kivymd.list import TwoLineListItem
 from kivymd.navigationdrawer import NavigationDrawerHeaderBase
 
+from pywallet.utils import load_kv_from_py
+
+
+load_kv_from_py(__file__)
+
 
 class NavigationDrawerTwoLineListItem(
         TwoLineListItem, NavigationDrawerHeaderBase):
