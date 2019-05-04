d422c4a6ac0e47118b7568d31fb750706fc11188
Andre Miras <andre.miras@gmail.com>
2019-05-05 00:00:35 +0200

    sha3 and scrypt are not in the deps anymore

--- src/tests/test_import.py
+++ src/tests/test_import.py
@@ -8,18 +8,7 @@ class ModulesImportTestCase(unittest.TestCase):
 
     def test_hashlib_sha3(self):
         import hashlib
-        import sha3
         self.assertIsNotNone(hashlib.sha3_512())
-        self.assertIsNotNone(sha3.keccak_512())
-
-    def test_scrypt(self):
-        import scrypt
-        # This will take at least 0.1 seconds
-        data = scrypt.encrypt('a secret message', 'password', maxtime=0.1)
-        self.assertIsNotNone(data)
-        # 'scrypt\x00\r\x00\x00\x00\x08\x00\x00\x00\x01RX9H'
-        decrypted = scrypt.decrypt(data, 'password', maxtime=0.5)
-        self.assertEqual(decrypted, 'a secret message')
 
     def test_zbarcam(self):
         from zbarcam import zbarcam
